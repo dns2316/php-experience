2d5f24fff479c443226934a81cc012e53f714753
dns2316 <dns2316@gmail.com>
2017-07-31 17:37:14 +0300

    connect to db postgresql. Make model.

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Movies;
 use Illuminate\Foundation\Bus\DispatchesJobs;
 use Illuminate\Routing\Controller as BaseController;
 use Illuminate\Foundation\Validation\ValidatesRequests;
@@ -10,4 +11,11 @@ use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 class Controller extends BaseController
 {
     use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
+}
+
+class JsonMovies extends BaseController{
+    public function show_json()
+    {
+        return Movies::up();
+    }
 }
