2b5d6eed665fb8b6ea5efff56acfd8b9c73cbfac
dns2316 <dns2316@gmail.com>
2017-08-02 17:44:55 +0200

    edit model and migrations

---
+++ database/migrations/2017_08_02_154258_Movie.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class Movie extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('movie', function (Blueprint $table){
+            $table->increments('id');
+            $table->string('name', 35);
+            $table->dateTimeTz('last_update');
+            $table->boolean('tracking');
+            $table->smallInteger('season');
+            $table->smallInteger('episode');
+            $table->string('url', 75);
+            $table->string('img', 255);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('movie');
+    }
+}
